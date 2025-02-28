be301c3b118e9dda12cf6025ad201bb11938fee1
Ariana <aocando@ggtech.es>
2025-02-27 22:12:42 -0400

    feat(components): add NavBar component for site navigation
    - Created `NavBar` component in `src/components/navBar/NavBar.tsx`
    - Added styles for `NavBar` in `src/scss/navbar.scss`
    - Integrated `LanguageSwitcher` and `ThemeButton` components into `NavBar`
    - Implemented navigation links using `HEADER_SECTIONS` from `src/config/constants.ts`
    - Added theme mode toggle functionality using `ThemeModeContext`

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,9 +1,18 @@
+import React from "react";
 import { useTranslation } from "react-i18next";
-import LanguageSwitcher from "../languageSwitcher/LanguageSwitcher";
-import { HEADER_SECTIONS } from "../../config/constants";
+import "@styles/navbar.scss";
+import { ThemeModeContext } from "../../contexts/ThemeModeContext.tsx";
+import { ThemeModeContextType } from "../../contexts/types.themeMode.ts";
+import LanguageSwitcher from "../languageSwitcher/LanguageSwitcher.tsx";
+import ToggleButton from "../themeButton/ThemeButton.tsx";
+import { HEADER_SECTIONS } from "../../config/constants.ts";
 
 function NavBar() {
-   const { t } = useTranslation();
+  const { t } = useTranslation();
+  const { isThemeDarkMode, changeThemeMode } = React.useContext(
+    ThemeModeContext
+  ) as ThemeModeContextType;
+
   return (
     <div className="navbar-content">
       <div className="navbar-content__item-container">
@@ -13,7 +22,13 @@ function NavBar() {
           </a>
         ))}
       </div>
-      <LanguageSwitcher />
+      <div className="navbar-content__button-container">
+        <ToggleButton
+          isDark={isThemeDarkMode as boolean}
+          onChange={changeThemeMode}
+        />
+        <LanguageSwitcher />
+      </div>
     </div>
   );
 }
