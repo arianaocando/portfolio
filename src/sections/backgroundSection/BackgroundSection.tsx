203d435e683fbfb6839b08222ccf33c21df89b21
Ariana <aocando@ggtech.es>
2025-02-27 22:02:18 -0400

    feat(sections): add BackgroundSection component for displaying timeline and featured content
    - Created `BackgroundSection` component in `src/sections/backgroundSection/BackgroundSection.tsx`
    - Added styles for `BackgroundSection` in `src/scss/BackgroundSection.scss`
    - Implemented timeline functionality with `TimelineItem` component
    - Displayed featured content using `BackgroundCard` component
    - Integrated i18n for translation support

--- src/sections/backgroundSection/BackgroundSection.tsx
+++ src/sections/backgroundSection/BackgroundSection.tsx
@@ -1,63 +1,39 @@
-import { useEffect, useRef, useState } from 'react';
-import image from "@assets/imagen.svg";
-
-import CardLarge from '@components/cardLarge/CardLarge';
-import TimelineItem from "@components/timeLineItem/TimeLineItem";
-import { timelineData } from '../../config/constants';
-import { useTranslation } from 'react-i18next';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import "@styles/BackgroundSection.scss";
+import image from "../../assets/jpg/company_logo_placeholder.jpg";
+import BackgroundCard from "@components/backgroundCard/BackgroundCard.tsx";
+import TimelineItem from "@components/timeLineItem/TimeLineItem.tsx";
+import { timelineData } from "../../config/constants.ts";
 
 function BackgroundSection() {
-  const [selectedItem, setSelectedItem] = useState(timelineData[0]);
+  const [selectedItem, setSelectedItem] = React.useState(timelineData[0]);
   const { t } = useTranslation();
-   const timelineRef = useRef(null);
-   const titleRef = useRef(null);
-
-   useEffect(() => {
-     if (timelineRef.current && titleRef.current) {
-       const timelineBottom =
-         timelineRef.current.getBoundingClientRect().bottom;
-       const titleTop = titleRef.current.getBoundingClientRect().top;
-       const newHeight = titleTop - timelineBottom + 660;
-
-      
-       document.documentElement.style.setProperty(
-         "--line-height",
-         `${newHeight}px`
-       );
-     }
-   }, [selectedItem]);
 
   return (
-    <section id="background" className="mb-5 timeline-section">
-      <div className="timeline-container">
-        <div className="featured-container">
-          <CardLarge
-            title={selectedItem.title}
-            subtitle={selectedItem.subtitle}
-            description={selectedItem.description}
-            imge={image}
-          />
-        </div>
-        <div className='timeline-cards'>
-
-        <div className="line"></div>
-        <div className="timeline" ref={timelineRef}>
-          {timelineData.map((item, index) => (
-            <TimelineItem
-              key={index}
-              year={item.year}
-              title={item.title}
-              subtitle={item.subtitle}
-              isSelected={selectedItem.year === item.year}
-              onSelect={() => setSelectedItem(item)}
-            />
-          ))}
-        </div>
+    <section id="background" className="timeline-section">
+      <div className="featured-container">
+        <BackgroundCard
+          title={selectedItem.title}
+          subtitle={selectedItem.subtitle}
+          description={selectedItem.description}
+          image={image}
+        />
+        <p className="timeline-title">{t("navbar.sections.background")}</p>
       </div>
-        </div>
-      <p className="timeline-title" ref={titleRef}>
-        {t('navbar.sections.background')}
-      </p>
+      <div className="line" />
+      <div className="timeline-cards">
+        {timelineData.map((item, index) => (
+          <TimelineItem
+            key={index}
+            year={item.year}
+            title={item.title}
+            subtitle={item.subtitle}
+            isSelected={selectedItem.year === item.year}
+            onSelect={() => setSelectedItem(item)}
+          />
+        ))}
+      </div>
     </section>
   );
 }
