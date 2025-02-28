ea9b6ea53f217e66a3ec7522f4932ada38863b1e
Ariana <aocando@ggtech.es>
2025-02-28 00:49:27 -0400

    refactor(sections): add ProjectSection component and styles
    - Refactored `ProjectSection` component in `src/sections/projectsSection/ProjectsSection.tsx`
    - Refactored styles for `ProjectSection` in `src/scss/ProjectsSection.scss`
    - Added renamed constants import extension

--- src/sections/projectsSection/ProjectsSection.tsx
+++ src/sections/projectsSection/ProjectsSection.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import "@styles/ProjectsSection.scss";
 import PillButton from "../../components/pillButton/PillButton.tsx";
-import { Project, projects } from "../../config/constants.ts";
+import { Project, projects } from "../../config/constants.tsx";
 
 function ProjectSection() {
   const [selectedProject, setSelectedProject] = React.useState<Project>(
@@ -13,48 +13,46 @@ function ProjectSection() {
     <section id="portfolio" className="portfolio">
       <p className="section-name">{t("navbar.sections.projects")}</p>
 
-      <div className="portfolio__container">
-        <div className="portfolio__list">
+      <div className="section-container">
+        <div className="list">
           {projects.map((project) => (
             <div
               key={project.id}
-              className={`portfolio__item ${
+              className={`item ${
                 selectedProject.id === project.id ? "active" : ""
               }`}
               onClick={() => setSelectedProject(project)}
             >
-              <div className="portfolio__item-info">
-                <p className="portfolio__item-title">{project.title}</p>
-                <p className="portfolio__item-resume">{project.title}</p>
+              <div className="item-info">
+                <p className="item-title">{project.title}</p>
+                <p className="item-resume">
+                  Tecnology use here is very special because of what i made and
+                  its very very special and i made it
+                </p>
               </div>
-              <div className="portfolio__image-container">
+              <div className="image-container">
                 <img
                   src={project.image}
                   alt={project.title}
-                  className="portfolio__item-image"
+                  className="item-image"
                 />
               </div>
             </div>
           ))}
         </div>
 
-        <div className="portfolio__details">
-          <div className="portfolio__details-image-container">
+        <div className="details">
+          <div className="image-container">
             <img
               src={selectedProject.image}
               alt={selectedProject.title}
-              className="portfolio__details-image"
+              className="image"
             />
           </div>
-          <h3 className="portfolio__details-title">{selectedProject.title}</h3>
-          <p className="portfolio__details-description">
-            {selectedProject.description}
-          </p>
-          <p className="portfolio__details-stack">Tecnologias de desarrollo</p>
-          <PillButton
-            skills={selectedProject.technologies}
-            activeClass={true}
-          />
+          <h3 className="title">{selectedProject.title}</h3>
+          <p className="description">{selectedProject.description}</p>
+          <p className="stack">Tecnologias de desarrollo</p>
+          <PillButton skills={selectedProject.technologies} activeClass />
         </div>
       </div>
     </section>
