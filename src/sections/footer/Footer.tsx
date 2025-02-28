f643ffc7def7759d525aa00455882d76badee699
Ariana <aocando@ggtech.es>
2025-02-28 00:45:46 -0400

    refactor(components): add Footer component for site footer
    - Refactored `Footer` component in `src/sections/footer/Footer.tsx`
    - Added styles for `Footer` in `src/scss/Footer.scss`
    - Implemented social network links using `socialNetworks` from `src/config/constants.tsx`
    - Added contact and description text to the footer

--- src/sections/footer/Footer.tsx
+++ src/sections/footer/Footer.tsx
@@ -1,26 +1,41 @@
+import "@styles/footer.scss";
 import { socialNetworks } from "../../config/constants";
-function Footer() {
 
+function Footer() {
   return (
     <footer className="footer-container">
-      <p className="contact-text">Contacto</p>
-      <p className="description-text">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora
-        nesciunt doloribus asperiores et, laudantium corrupti! Impedit,
-        accusantium facilis quisquam perferendis deleniti unde eius itaque
-        ipsam, dolorem id pariatur distinctio amet.
-      </p>
-      <div className="footer-buttons">
-        {socialNetworks.map((social, index) => (
-          <button
-            className="btn btn-secondary shadow-sm"
-            type="submit"
-            key={index}
-          >
-            <img src={social.icon} alt={social.name} />
-            <a href={social.url}>{social.name}</a>
-          </button>
-        ))}
+      <div className="content-container">
+        <p className="contact-text">Contacto</p>
+        <p className="description-text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora
+          nesciunt doloribus asperiores et, laudantium corrupti! Impedit,
+          accusantium facilis quisquam perferendis deleniti unde eius itaque
+          ipsam, dolorem id pariatur distinctio amet.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora
+          nesciunt doloribus asperiores et, laudantium corrupti! Impedit,
+          accusantium facilis quisquam perferendis deleniti unde eius itaque
+          ipsam, dolorem id pariatur distinctio amet.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora
+          nesciunt doloribus asperiores et, laudantium corrupti! Impedit,
+          accusantium facilis quisquam perferendis deleniti unde eius itaque
+          ipsam, dolorem id pariatur distinctio amet.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora
+          nesciunt doloribus asperiores et, laudantium corrupti! Impedit,
+          accusantium facilis quisquam perferendis deleniti unde eius itaque
+          ipsam, dolorem id pariatur distinctio amet.
+        </p>
+        <div className="footer-buttons">
+          {socialNetworks.map((social, index) => (
+            <button
+              className="btn btn-secondary"
+              type="submit"
+              key={index}
+            >
+              <img src={social.icon} alt={social.name} />
+              <a href={social.url}>{social.name}</a>
+            </button>
+          ))}
+        </div>
       </div>
       <p className="develop-name">Ariana Ocando™ 2025</p>
     </footer>
