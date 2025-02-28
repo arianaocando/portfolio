3d661958449fa7e53b44dfed89c22e469856000f
Ariana <aocando@ggtech.es>
2025-02-27 22:14:35 -0400

    feat(sections): add BannerSection component for displaying hero banner
    - Created `BannerSection` component in `src/sections/bannerSection/BannerSection.tsx`
    - Added styles for `BannerSection` in `src/scss/BannerSection.scss`
    - Implemented dynamic banner image switching based on theme mode
    - Integrated i18n for translation support

--- src/sections/bannerSection/BannerSection.tsx
+++ src/sections/bannerSection/BannerSection.tsx
@@ -1,11 +1,26 @@
+import React from "react";
 import { useTranslation } from "react-i18next";
 import heroBanner from "@assets/hero-banner.svg";
+import heroBannerDark from "@assets/dark-theme-image/banner_dark.svg";
+import { ThemeModeContext } from "../../contexts/ThemeModeContext.tsx";
+import { ThemeModeContextType } from "../../contexts/types.themeMode.ts";
+import "@styles/BannerSection.scss";
 
 function BannerSection() {
   const { t } = useTranslation();
+  const { isThemeDarkMode } = React.useContext(
+    ThemeModeContext
+  ) as ThemeModeContextType;
+
   return (
-    <section id="banner" className="banner-section position-relative mt-4 d-flex flex-column justify-content-end">
-      <img src={heroBanner} className="img-fluid"></img>
+    <section
+      id="banner"
+      className="banner-section position-relative d-flex flex-column justify-content-end"
+    >
+      <img
+        src={isThemeDarkMode ? heroBannerDark : heroBanner}
+        className="img-fluid"
+      />
       <div className="banner-section__content">
         <h1>{t("banner.name")}</h1>
         <h1 className="lastname">Ocando</h1>
