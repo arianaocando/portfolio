a80c42c28fad3002a7aa66897b1a1867fef28985
Ariana <aocando@ggtech.es>
2025-02-27 22:04:07 -0400

    feat(sections): add PositionSection component for displaying job title and description
    - Created `PositionSection` component in `src/sections/positionSection/PositionSection.tsx`
    - Added styles for `PositionSection` in `src/scss/PositionSection.scss`
    - Implemented functionality to display job title and description

--- src/sections/positionSection/PositionSection.tsx
+++ src/sections/positionSection/PositionSection.tsx
@@ -1,18 +1,22 @@
+import "@styles/PositionSection.scss";
+
 function PositionSection() {
   return (
-    <section id="position" className="position-section__content">
+    <section id="position" className="position-section">
+      <div className="position-section__content">
         <div className="position-section__title">
-            <p className="position-section__full-stack">Semi-Senior Full-Stack</p>
-            <p className="position-section__developer">Software Developer</p>
+          <p className="position-section__full-stack">Semi-Senior Full-Stack</p>
+          <p className="position-section__developer">Software Developer</p>
         </div>
         <p className="position-section__description">
-            Salami incididunt dolore andouille cupidatat elit aliquip id sausage
-            spare ribs turkey tempor. Deserunt bacon cupim, voluptate consequat
-            sed venison nisi aliquip tri-tip. Pancetta cow ipsum incididunt,
-            leberkas picanha lorem chuck. Dolore lorem tongue, nisi landjaeger ut
-            fatback capicola t-bone kevin pastrami. Kevin ham hock buffalo
-            voluptate.
-          </p>
+          Salami incididunt dolore andouille cupidatat elit aliquip id sausage
+          spare ribs turkey tempor. Deserunt bacon cupim, voluptate consequat
+          sed venison nisi aliquip tri-tip. Pancetta cow ipsum incididunt,
+          leberkas picanha lorem chuck. Dolore lorem tongue, nisi landjaeger ut
+          fatback capicola t-bone kevin pastrami. Kevin ham hock buffalo
+          voluptate.
+        </p>
+      </div>
     </section>
   );
 }
