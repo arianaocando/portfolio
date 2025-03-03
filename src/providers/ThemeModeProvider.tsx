d056907d0ee4464cc8a2a35610e149eabe36c682
Ariana <aocando@ggtech.es>
2025-03-02 22:07:01 -0400

    refactor: clean up code formatting and improve readability in translation and ThemeModeProvider
    - Improved code formatting and readability in `src/locales/es/translation.json`
    - Cleaned up and refactored `ThemeModeProvider` in `src/providers/ThemeModeProvider.tsx`
    - Ensured consistent code style and structure across the files

--- src/providers/ThemeModeProvider.tsx
+++ src/providers/ThemeModeProvider.tsx
@@ -5,35 +5,34 @@ import { ThemeModeContext } from "../contexts/ThemeModeContext.tsx";
 const metaThemePropertyName = "ariana-ocando-dark-theme";
 
 function ThemeModeProvider({ children }: { children: React.ReactNode }) {
-    const [isThemeDarkMode, setIsThemeDarMode] =
-        React.useState<boolean>(false);
-    
-    const setThemeMode = () => { 
-        setIsThemeDarMode(
-          window.matchMedia("(prefers-color-scheme: dark)").matches
-        );
-    };
+  const [isThemeDarkMode, setIsThemeDarMode] = React.useState<boolean>(false);
 
-    const changeThemeMode = () => {
-        const documentThemeIsDark = document.body.getAttribute(
-        metaThemePropertyName
-        );
-        if (documentThemeIsDark === ThemeModeEnum.DARK) {
-            document.body.setAttribute(metaThemePropertyName, ThemeModeEnum.LIGHT);
-            setIsThemeDarMode(false);
-        } else {
-            document.body.setAttribute(metaThemePropertyName, ThemeModeEnum.DARK);
-            setIsThemeDarMode(true);
-        }
-    }
-    
-    return (
-      <ThemeModeContext.Provider
-        value={{ isThemeDarkMode, changeThemeMode, setThemeMode }}
-      >
-        {children}
-      </ThemeModeContext.Provider>
+  const setThemeMode = () => {
+    setIsThemeDarMode(
+      window.matchMedia("(prefers-color-scheme: dark)").matches
     );
+  };
+
+  const changeThemeMode = () => {
+    const documentThemeIsDark = document.body.getAttribute(
+      metaThemePropertyName
+    );
+    if (documentThemeIsDark === ThemeModeEnum.DARK) {
+      document.body.setAttribute(metaThemePropertyName, ThemeModeEnum.LIGHT);
+      setIsThemeDarMode(false);
+    } else {
+      document.body.setAttribute(metaThemePropertyName, ThemeModeEnum.DARK);
+      setIsThemeDarMode(true);
+    }
+  };
+
+  return (
+    <ThemeModeContext.Provider
+      value={{ isThemeDarkMode, changeThemeMode, setThemeMode }}
+    >
+      {children}
+    </ThemeModeContext.Provider>
+  );
 }
 
 export default ThemeModeProvider;
